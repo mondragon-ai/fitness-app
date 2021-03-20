d63232307efaefdabe971d3b4c5063054745008d
mondragon-ai <angel@plutusx.com>
2021-03-19 20:14:39 -0600

    basic styling, navbar, toolbar

---
+++ fitness-application/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import ToolBar from './components/ToolBar';
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+      <div className="component">
+
+      </div>
+      <ToolBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ fitness-application/src/components/ToolBar.js
@@ -0,0 +1,12 @@
+const ToolBar = () => {
+    return (
+        <div className="toolbar">
+            <h1>home</h1>
+            <h1>workouts</h1>
+            <h1>body metrics</h1>
+            <h1>wall</h1>
+        </div>
+    )
+}
+
+export default ToolBar;
