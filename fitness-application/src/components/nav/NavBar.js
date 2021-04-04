b168bf4812c30c4c60826d15f5347c7efb9793d3
mondragon-ai <angel@plutusx.com>
2021-04-03 21:10:37 -0600

    dash - quick stats - bars - no styling

---
+++ fitness-application/src/components/nav/NavBar.js
@@ -0,0 +1,11 @@
+
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <h1>timer</h1>
+            <h1>profile</h1>
+        </nav>
+    )
+}
+
+export default NavBar; 
