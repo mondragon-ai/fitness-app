c993af3486f1d2cc1a61974ec5d162be3407a025
mondragon-ai <angel@plutusx.com>
2021-04-03 21:08:28 -0600

    Cleaned Dash & Added jsx - pre components. + file structure

---
+++ fitness-application/src/components/dash/QuickAdd.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function QuickAdd() {
+    return (
+        <div className="grid-container">
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+            <div className="grid-item"></div>
+
+        </div> 
+    )
+}
+
+export default QuickAdd
